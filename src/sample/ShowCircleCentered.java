cd3fcf788e760878986d6d6c93716351103a3603
MathiasMadsen <m1999@live.dk>
2020-11-24 13:15:05 +0100

    ShowCircleCentered

---
+++ src/sample/ShowCircleCentered.java
@@ -0,0 +1,55 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class ShowCircleCentered extends Application {
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Create a pane to hold the circle
+        Pane pane = new Pane();
+
+        // Create a circle and set its properties
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+        pane.getChildren().add(circle); // Add circle to the pane
+
+        Circle circle2 = new Circle();
+        circle2.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle2.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle2.setRadius(25);
+        circle2.setStroke(Color.BLACK);
+        circle2.setFill(Color.GREEN);
+        pane.getChildren().add(circle2); // Add circle to the pane
+
+        Circle circle3 = new Circle();
+        circle3.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle3.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle3.setRadius(12.5);
+        circle3.setStroke(Color.BLACK);
+        circle3.setFill(Color.RED);
+        pane.getChildren().add(circle3); // Add circle to the pane
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 400, 200);
+        primaryStage.setTitle("ShowCircleCentered"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /***
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     ***/
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
